dc570d1ad5b267107351f1a820a2bf4dd7f74e9c
Jonatan Arrocha Kang <erickangweisz@gmail.com>
2019-08-14 21:50:37 +0100

    declare variables and edit constructor in navbar service and add this service into index file

--- src/app/_components/navbar/navbar.component.ts
+++ src/app/_components/navbar/navbar.component.ts
@@ -1,12 +1,23 @@
 import { Component, OnInit } from '@angular/core'
+import { AuthenticationService } from 'src/app/_services'
+import { NavbarService } from 'src/app/_services'
+
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.sass']
+  styleUrls: ['./navbar.component.sass'],
+  providers: [ NavbarService, AuthenticationService ]
 })
 export class NavbarComponent implements OnInit {
+  title: string
+  currentUser: any
 
-  constructor() {}
+  constructor(
+    private _authService: AuthenticationService, 
+    private _navbarService: NavbarService
+  ) {
+    this.title = 'LVL-ART'
+  }
 
   ngOnInit() {}
 
--- src/app/_services/index.ts
+++ src/app/_services/index.ts
@@ -1,2 +1,3 @@
 export { AuthenticationService } from './authentication/authentication.service'
+export { NavbarService } from './navbar/navbar.service'
 export { UserService } from './user/user.service'
