c13387e680572fdd76331574d3de4bc3684187bc
Jonatan Arrocha Kang <erickangweisz@gmail.com>
2019-08-16 18:49:44 +0100

    remove loginform to app-routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,16 +2,11 @@ import { NgModule } from '@angular/core'
 import { Routes, RouterModule } from '@angular/router'
 
 import { HomeComponent } from './_components'
-import { LoginFormComponent } from './_components'
 import { AuthGuard } from './_guards'
 
 const routes: Routes = [
   {
     path: 'home',
-    component: HomeComponent
-  },
-  {
-    path: 'login',
     component: HomeComponent
   },
   // otherwise redirect to home
